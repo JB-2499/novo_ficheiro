dbcc33833195a8a9d45897556bb9fbb394e43070
João Barreto <Joo.bcsilva@upe.br>
2025-12-22 13:12:19 -0300

    Criar projeto

---
+++ src/main/java/joao_barreto/novo_ficheiro/NovoFicheiroApplication.java
@@ -0,0 +1,13 @@
+package joao_barreto.novo_ficheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoFicheiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovoFicheiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/joao_barreto/novo_ficheiro/NovoFicheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package joao_barreto.novo_ficheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoFicheiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
